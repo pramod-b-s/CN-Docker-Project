c82f45fab314b34d35a18b8b43ec76224c00520a
pramod-b-s <15co234.pramod@nitk.edu.in>
2017-10-30 18:44:52 +0530

    kernel_change_scripts_added

---
+++ working-directory/scripts/install64.sh
@@ -0,0 +1,23 @@
+# Script to install Linux 3.17 kernel on 64 bit Ubuntu system 
+
+#!/bin/bash
+sudo wget http://kernel.ubuntu.com/~kernel-
+ppa/mainline/v3.17-utopic/linux-headers-3.17.0-031700-
+generic_3.17.0-031700.201410060605_amd64.deb
+
+#!/bin/bash
+sudo wget http://kernel.ubuntu.com/~kernel-
+ppa/mainline/v3.17-utopic/linux-headers-3.17.0-031700_3.17.0-
+031700.201410060605_all.deb
+
+#!/bin/bash
+sudo wget http://kernel.ubuntu.com/~kernel-
+ppa/mainline/v3.17-utopic/linux-image-3.17.0-031700-
+generic_3.17.0-031700.201410060605_amd64.deb
+
+#!/bin/bash
+sudo dpkg -i linux-headers-3.17.0-*.deb linux-image-
+3.17.0-*.deb
+
+#!/bin/bash
+sudo reboot
