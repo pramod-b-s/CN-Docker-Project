685ddf99cb6c596faa05c66e7c670c481cfa4419
pramod-b-s <15co234.pramod@nitk.edu.in>
2017-11-03 22:40:10 +0530

    removed-wget-cmnds

--- working-directory/scripts/install64.sh
+++ working-directory/scripts/install64.sh
@@ -1,19 +1,14 @@
-# Script to install Linux 3.17 kernel on 64 bit Ubuntu system 
+# Script to install Linux 3.17 kernel on 64 bit Ubuntu system
 
 #!/bin/bash
-sudo wget http://kernel.ubuntu.com/~kernel-
-ppa/mainline/v3.17-utopic/linux-headers-3.17.0-031700-
-generic_3.17.0-031700.201410060605_amd64.deb
+cp 64-bit-linux-kernel/linux-headers-3.17.0-031700-generic_3.17.0-031700.201410060605_amd64.deb /tmp/
 
-sudo wget http://kernel.ubuntu.com/~kernel-
-ppa/mainline/v3.17-utopic/linux-headers-3.17.0-031700_3.17.0-
-031700.201410060605_all.deb
+cp 64-bit-linux-kernel/linux-headers-3.17.0-031700_3.17.0-031700.201410060605_all.deb /tmp/
 
-sudo wget http://kernel.ubuntu.com/~kernel-
-ppa/mainline/v3.17-utopic/linux-image-3.17.0-031700-
-generic_3.17.0-031700.201410060605_amd64.deb
+cp 64-bit-linux-kernel/linux-image-3.17.0-031700-generic_3.17.0-031700.201410060605_amd64.deb /tmp/
 
-sudo dpkg -i linux-headers-3.17.0-*.deb linux-image-
-3.17.0-*.deb
+cd /tmp/
+
+sudo dpkg -i linux-headers-3.17.0-*.deb linux-image-3.17.0-*.deb
 
 sudo reboot
--- working-directory/scripts/installteacup.sh
+++ working-directory/scripts/installteacup.sh
@@ -1,24 +1,15 @@
 # Script to install Web10g and run TEACUP Docker container
 
 #!/bin/bash
-cd /usr/src
+cp patches/linux-3.17.4 /usr/src/
 
-wget https://www.linux-mips.org/pub/linux/mips/kernel/v3.x/linux-3.17.4.tar.xz
+cp patches/web10g-userland-2.0.9 /usr/src/linux-3.17.4/
 
-tar -xvf linux-3.17.4.tar.xz
-
-cd linux-3.17.4
-
-wget https://www.web10g.org/images/Software/Web10G_Kernel_Patch_and_API_Packages/web10g-userland-2.0.9.tar.gz
-
-tar -xvf web10g-userland-2.0.9.tar.gz
-
-cd web10g-userland-2.0.9
-
-cd ..
+cd /usr/src/linux-3.17.4/
 
 patch -p1 < /usr/src/linux-3.17.4/web10g-userland-2.0.9/web10g-0.11-3.17.diff
 
-sudo docker import teacupimage.tar teacupdocker:teacup
-
-sudo docker run -it teacupdocker:teacup
+sudo docker load -i teacupimage.tar.gz
+# save the image with the name "teacupcontainer"
+# save command --> docker save -o teacupimage.tar.gz teacupcontainer
+sudo docker run -it teacupcontainer
